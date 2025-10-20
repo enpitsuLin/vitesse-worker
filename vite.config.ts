4118f8facb6af2022a3dd1e567ac96840bd5d4cc
enpitsulin <enpitsulin@gmail.com>
2025-10-20 21:03:08 +0800

    fix: workbox exclude html

--- vite.config.ts
+++ vite.config.ts
@@ -155,6 +155,14 @@ export default defineConfig({
           },
         ],
       },
+      workbox: {
+        // Only precache these files - html should be excluded
+        globPatterns: ['**/*.{js,css}'],
+
+        // Don't fallback on document based (e.g. `/some-page`) requests
+        // Even though this says `null` by default, I had to set this specifically to `null` to make it work
+        navigateFallback: null,
+      },
     }),
 
     // https://github.com/intlify/bundle-tools/tree/main/packages/unplugin-vue-i18n
