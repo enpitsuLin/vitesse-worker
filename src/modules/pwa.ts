cea0a52ee38eb85e6c945c91f9eff1cf0e720445
enpitsulin <enpitsulin@gmail.com>
2025-10-20 20:33:03 +0800

    fix: pwa manifest

--- src/modules/pwa.ts
+++ src/modules/pwa.ts
@@ -1,4 +1,7 @@
+import type { ResolvableLink } from '@unhead/vue'
 import type { UserModule } from '~/types'
+import { pwaInfo } from 'virtual:pwa-info'
+import { defineComponent, ref } from 'vue'
 
 // https://github.com/antfu/vite-plugin-pwa#automatic-reload-when-new-content-available
 export const install: UserModule = ({ router }) => {
@@ -10,5 +13,34 @@ export const install: UserModule = ({ router }) => {
       const { registerSW } = await import('virtual:pwa-register')
       registerSW({ immediate: true })
     })
-    .catch(() => {})
+    .catch(() => { })
 }
+
+export const PWAManifest = defineComponent({
+  setup() {
+    if (pwaInfo) {
+      const meta = ref<{ link: ResolvableLink[] }>({ link: [] })
+      useHead(meta)
+
+      const { webManifest } = pwaInfo
+      if (webManifest) {
+        const { href, useCredentials } = webManifest
+        if (useCredentials) {
+          meta.value.link!.push({
+            rel: 'manifest',
+            href,
+            crossorigin: 'use-credentials',
+          })
+        }
+        else {
+          meta.value.link!.push({
+            rel: 'manifest',
+            href,
+          })
+        }
+      }
+    }
+
+    return () => null
+  },
+})
